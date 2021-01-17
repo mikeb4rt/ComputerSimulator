41a95d85be827dc8a529fc9a26a24e3c42f382ea
mikeb4rt <rodrigosern@gmail.com>
2021-01-17 19:05:42 +0100

    cambios de idioma y comentarios

--- src/GuessNumber/Guess.java
+++ src/GuessNumber/Guess.java
@@ -1,6 +1,6 @@
 package GuessNumber;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Guess {
@@ -9,18 +9,7 @@ public class Guess {
     private int userNumber;
     private int tryes = 5;
     private static int money = 20;
-    boolean salir = false;
-
-    //Setters
-    public void setuserNumber() {
-        System.out.print("Introduzca un numero: ");
-        Scanner user = new Scanner(System.in);
-        this.userNumber = user.nextInt();
-    }
-
-    public void PagaTryes(){
-        this.tryes +=1;
-    }
+    boolean exit = false;
 
     //Getters
     public int getGenNumber(){
@@ -32,63 +21,76 @@ public class Guess {
     public int getMoney(){
         return money;
     }
-
     public int getUserNumber(){
         return Integer.parseInt(String.valueOf(this.userNumber));
     }
 
-    //Methods
-    public void reduceTryes(){
-        this.tryes -= 1;
+    //Setters
+    public void setuserNumber() {
+        System.out.print("Introduzca un numero: ");
+        Scanner user = new Scanner(System.in);
+        this.userNumber = user.nextInt();
     }
 
     public void sumMoney(int num){
         money += num;
     }
 
-    public boolean check(){
-        if (money > 0) {
-        int[] introducednumbers = new int [5];
-        int i = 0;
-        sumMoney(1);
-            do {
-                    this.setuserNumber();
-                    this.reduceTryes();
-                    introducednumbers[i] = userNumber;
-                    System.out.println(this.getGenNumber());
-                    if (this.guess()) {
-                        System.out.println("Numero acertado");
-                        System.out.println("Numero a acertar " + this.getGenNumber());
-                        System.out.println("Numero a introducido " + this.getUserNumber());
-                        this.sumMoney(-5);
-                        System.out.println(this.getMoney());
-                        salir = true;
-                        for (int introducednumber : introducednumbers) {
-                            System.out.print(introducednumber + ", ");
-                        }
-                        return true;
-                    } else {
-                        if (this.getGenNumber() < this.getUserNumber()) {
-                            System.out.println("El numero a acertar es menor a " + this.getUserNumber() + " intentelo de nuevo");
-                        } else
-                            System.out.println("El numero a acertar es mayor a " + this.getUserNumber() + " intentelo de nuevo");
-                    }
-                    i++;
-            }while ((!this.guess()) && (this.getTryes() > 0)) ;
-                System.out.print("Numeros introducidos: ");
-                for (int introducednumber : introducednumbers) {
-                    System.out.print(introducednumber + ", ");
-                }
-        }else System.out.println("Maquina sin dinero");
-            return false;
-    }
 
+    //Methods
+
+    //Este metodo comprueba si el numero que ha introducido el usuario es el mismo que el que ha generado el programa aleatoriamente
     public boolean guess(){
         return this.userNumber == this.genNumber;
     }
 
-    public void tragaperras() {
-        while (!salir){
+    //Este metodo es el que controla el funcionamiento del juego
+    public boolean check(){
+        //Si la maquina tiene dinero suficiente para pagar en caso de que se hacierte el numero seguira adelante con el juego
+        if (money > 5) {
+            //Creamos un ArrayList en la que se almacenaran los numeros introducidos por el usuario
+            ArrayList<Integer> introducednumbers = new ArrayList<>();
+            //Sumamos el dinero que paga el usuario por jugar
+            sumMoney(1);
+                do {
+                        //Pedimos al usuario que introduzca un numero
+                        this.setuserNumber();
+                        this.tryes --;
+                        //Añadimos el numero que ha introducido el usuario al array
+                        introducednumbers.add(userNumber);
+                        //Comprovamos que el usuario haya acertado el numero
+                        if (this.guess()) {
+                            System.out.println("Numero acertado");
+                            System.out.println("Numero a acertar " + this.getGenNumber());
+                            System.out.println("Numero a introducido " + this.getUserNumber());
+                            this.sumMoney(-5);
+                            System.out.println(this.getMoney());
+                            exit = true;
+                            return true;
+                        //Si no lo ha acertado le decimos si el numero es mas alto o mas bajo al que ha introducido el usuario
+                        } else {
+                            if (this.getGenNumber() < this.getUserNumber()) {
+                                System.out.println("El numero a acertar es menor a " + this.getUserNumber() + " intentelo de nuevo");
+                            } else
+                                System.out.println("El numero a acertar es mayor a " + this.getUserNumber() + " intentelo de nuevo");
+                        }
+                        //Imprimimos por pantalla los numeros que ha introducido el usuario
+                        System.out.print("Numeros introducidos: ");
+                         for (int introducednumber : introducednumbers) {
+                            System.out.print(introducednumber + ", ");
+                         }
+                }while ((!this.guess()) && (this.getTryes() > 0));
+        }else{
+            System.out.println("Maquina sin dinero");
+            exit = true;
+        }
+        return false;
+    }
+
+    //Este metodo hara de menu principal del programa
+    public void slot() {
+        while (!exit){
+            //Comprobamos si se acierta el numero y generamos un menu
             if(!this.check()){
                 System.out.println("Quiere 1 intento mas?");
                 System.out.println("1 --> SI");
@@ -101,7 +103,7 @@ public class Guess {
                 }else  {
                     System.out.println("Hasta la proxima");
                     System.out.println(this.getMoney());
-                    salir = true;
+                    exit = true;
                 }
             }
         }
--- src/GuessNumber/Main.java
+++ src/GuessNumber/Main.java
@@ -3,6 +3,6 @@ package GuessNumber;
 public class Main {
     public static void main(String[] args) {
         Guess t1 = new Guess();
-        t1.tragaperras();
+        t1.slot();
     }
 }
